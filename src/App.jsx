ebb2625c13dd0884014a7237234d97eabeb1e99a
Carlos Y. Ruiz T. <cyair99@gmail.com>
2024-11-15 20:27:44 -0500

    Se agrego todo el nivel 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Level1 from "./components/Level1";
+import Level5 from "./components/Level5";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/level1" element={<Level1 />} />
+        <Route path="/level5" element={<Level5/>} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
