78f0388b2c7377a23b5b3e361e572a1c9980daad
marcosd59 <damian.marcospool@gmail.com>
2024-11-20 22:24:04 -0500

    Add Tutorial route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import History from "./components/History";
+import Tutorial from "./components/Tutorial";
 import Level1 from "./components/Level1";
 import Level4 from "./components/Level4";
 import Level5 from "./components/Level5";
@@ -11,9 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/historia" element={<History />} />
+        <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/level1" element={<Level1 />} />
         <Route path="/level4" element={<Level4 />} />
-        <Route path="/level5" element={<Level5/>} />
+        <Route path="/level5" element={<Level5 />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
